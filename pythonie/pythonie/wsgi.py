c120254ea499ff8dc050d3e8491eeeadf2376cf1
Diarmuid Bourke <diarmuidbourke@gmail.com>
2015-06-13 15:11:59 +0100

    Serve static files with Cling from dj_static

--- pythonie/pythonie/wsgi.py
+++ pythonie/pythonie/wsgi.py
@@ -11,6 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonie.settings.production")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+from dj_static import Cling
+application = Cling(get_wsgi_application())
