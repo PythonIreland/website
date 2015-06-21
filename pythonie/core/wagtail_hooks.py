415a61029436405333f19803767871e68dff2025
Diarmuid Bourke <diarmuidbourke@gmail.com>
2015-06-21 13:15:50 +0100

    Bypassing stupidity. Allow iframes in html.

--- pythonie/core/wagtail_hooks.py
+++ pythonie/core/wagtail_hooks.py
@@ -1,5 +1,7 @@
 from django.utils.html import format_html
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailcore.whitelist import attribute_rule
+
 
 @hooks.register('insert_editor_js')
 def enable_source():
@@ -10,3 +12,18 @@ def enable_source():
         </script>
         """
     )
+
+
+@hooks.register('construct_whitelister_element_rules')
+def allow_iframes():
+    return {
+        'iframe': attribute_rule(
+            {
+                'src': True,
+                'width': True,
+                'height': True,
+                'frameborder': True,
+                'marginheight': True,
+                'marginwidth': True
+            })
+    }
