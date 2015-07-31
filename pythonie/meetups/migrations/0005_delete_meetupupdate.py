cf3859f34aa0825582ab22c075d8a3d89b8bdef4
Diarmuid Bourke <diarmuidbourke@gmail.com>
2015-07-31 16:22:37 +0100

    Migration to remove the meetupupdate model

---
+++ pythonie/meetups/migrations/0005_delete_meetupupdate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('meetups', '0004_auto_20150705_1641'),
+    ]
+
+    operations = [
+        migrations.DeleteModel(
+            name='MeetupUpdate',
+        ),
+    ]
