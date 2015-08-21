c8a15a42b910f154b8acb32bda1dab5651b697ec
Diarmuid Bourke <diarmuidbourke@gmail.com>
2015-08-22 00:07:07 +0100

    Fix up migration

--- pythonie/speakers/migrations/0001_initial.py
+++ pythonie/speakers/migrations/0001_initial.py
@@ -7,14 +7,14 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0002_auto_20150812_1417'),
+        ('wagtailcore', '0002_auto_20150821_2305'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='SpeakersPage',
             fields=[
-                ('page_ptr', models.OneToOneField(to='wagtailcore.Page', primary_key=True, serialize=False, parent_link=True, auto_created=True)),
+                ('page_ptr', models.OneToOneField(primary_key=True, serialize=False, to='wagtailcore.Page', parent_link=True, auto_created=True)),
                 ('api_url', models.CharField(max_length=255)),
             ],
             options={
