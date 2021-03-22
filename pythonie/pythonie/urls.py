e6808f2547667e8d236be556eb99a2d1a22ba9b8
Stéphane Wirtel <stephane@wirtel.be>
2021-03-22 20:27:28 +0100

    Disable wagtail.search

--- pythonie/pythonie/urls.py
+++ pythonie/pythonie/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.search import urls as wagtailsearch_urls
+# from wagtail.search import urls as wagtailsearch_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
 
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^django-admin/', admin.site.urls),
     url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^search/', include(wagtailsearch_urls)),
+#    url(r'^search/', include(wagtailsearch_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'', include(wagtail_urls))
 ]
