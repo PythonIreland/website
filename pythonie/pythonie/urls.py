5bd2b8e8aa1c2d871ee81e4206a23c56b1bc13ed
Stéphane Wirtel <stephane@wirtel.be>
2021-03-22 20:59:10 +0100

    blackify

--- pythonie/pythonie/urls.py
+++ pythonie/pythonie/urls.py
@@ -6,23 +6,25 @@ from django.conf import settings
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
+
 # from wagtail.search import urls as wagtailsearch_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
-    url(r'^django-admin/', admin.site.urls),
-    url(r'^admin/', include(wagtailadmin_urls)),
-#    url(r'^search/', include(wagtailsearch_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'', include(wagtail_urls))
+    url(r"^blog/", include("blog.urls")),
+    url(r"^django-admin/", admin.site.urls),
+    url(r"^admin/", include(wagtailadmin_urls)),
+    #    url(r'^search/', include(wagtailsearch_urls)),
+    url(r"^documents/", include(wagtaildocs_urls)),
+    url(r"", include(wagtail_urls)),
 ]
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL + 'images/',
-                          document_root=os.path.join(settings.MEDIA_ROOT,
-                                                     'images'))
+    urlpatterns += static(
+        settings.MEDIA_URL + "images/",
+        document_root=os.path.join(settings.MEDIA_ROOT, "images"),
+    )
