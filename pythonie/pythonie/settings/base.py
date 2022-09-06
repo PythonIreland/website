aedd079cd0f58e3bb7618f03fe15681d0e10cb3b
Stéphane Wirtel <stephane@wirtel.be>
2022-09-06 21:56:21 +0200

    Refactoring: Use a dict construct for the default database

--- pythonie/pythonie/settings/base.py
+++ pythonie/pythonie/settings/base.py
@@ -134,9 +134,7 @@ MEETUP_KEY = os.getenv("MEETUP_KEY")
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 
-DATABASES = {}
-
-DATABASES["default"] = dj_database_url.config()
+DATABASES = {"default": dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
