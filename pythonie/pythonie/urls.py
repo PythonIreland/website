0d148d01691f4338e205af305721c2056e14bd45
Stéphane Wirtel <stephane@wirtel.be>
2024-05-31 14:44:06 +0200

    upgrade to django 4.0.10 and pydantic 1.10.15

--- pythonie/pythonie/urls.py
+++ pythonie/pythonie/urls.py
@@ -1,7 +1,8 @@
 import os
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import path, re_path
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
@@ -11,11 +12,11 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    url(r"^django-admin/", admin.site.urls),
-    url(r"^admin/", include(wagtailadmin_urls)),
+    re_path(r"^django-admin/", admin.site.urls),
+    re_path(r"^admin/", include(wagtailadmin_urls)),
     #    url(r'^search/', include(wagtailsearch_urls)),
-    url(r"^documents/", include(wagtaildocs_urls)),
-    url(r"", include(wagtail_urls)),
+    re_path(r"^documents/", include(wagtaildocs_urls)),
+    path(r"", include(wagtail_urls)),
 ]
 
 if settings.DEBUG:
