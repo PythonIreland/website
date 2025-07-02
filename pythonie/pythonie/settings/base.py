bb4a763090c8e3945b0b8a56b67d882c53e36aed
Stéphane Wirtel <stephane@wirtel.be>
2025-07-02 11:56:55 +0200

    Remove support for speakers and sessions

--- pythonie/pythonie/settings/base.py
+++ pythonie/pythonie/settings/base.py
@@ -14,6 +14,7 @@ import dj_database_url
 
 # Absolute filesystem path to the Django project directory:
 PROJECT_ROOT = dirname(dirname(dirname(abspath(__file__))))
+print(f'{PROJECT_ROOT=}')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -61,7 +62,6 @@ INSTALLED_APPS = (
     "storages",
     "meetups",
     "sponsors",
-    "speakers",
     # 'debug_toolbar',
     "django_extensions",
 )
@@ -112,11 +112,6 @@ LOGGING = {
             "propagate": True,
         },
         "sponsors": {
-            "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
-            "propagate": True,
-        },
-        "speakers": {
             "handlers": ["console"],
             "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
             "propagate": True,
